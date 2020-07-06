6309c8ce3e2082049a4ebfc11f4494ca8f7a667a
VladKoshkin <xenomorph-balash@yandex.ru>
2020-07-06 15:53:42 +0300

    Урок 3, Java 2

--- HomeWork/src/main/java/ru/geekbrains/Java2/MainX.java
+++ HomeWork/src/main/java/ru/geekbrains/Java2/MainX.java
@@ -1,31 +1,60 @@
 package ru.geekbrains.Java2;
 
-public class MainX {
-    public static void main(String[] args) throws Not4By4Exception, NumberFormatException, NullPointerException {
-        String s = "10 3 1 2\n2 3 2 2\n5 6 7 1\n300 3 1 0";
-        String[][] spitedStringArray=null;
-        StringToArray worker=new StringToArray(s);
-        try {
-            spitedStringArray=worker.splitArray();   // разбиваем на массив типа String[][]
-            System.out.println("Массив String[][]:");
-            worker.printStringArray(spitedStringArray);         // печатаем получившийся массив
-        }
-        catch (Not4By4Exception e) {
-            System.out.println(e.getMessage());
-        }
+import java.util.ArrayList;
 
-        try {
-            int[][] spitedIntArray = worker.transferArray(spitedStringArray);   // преобразуем в массив типа int[][]
-            System.out.println("Массив int[][]:");
-            worker.printIntArray(spitedIntArray);   // распечатываем полученный массив
-            System.out.println(worker.arithmeticActions(spitedIntArray));   // делаем необходимые арифметические операции
+public class MainX {
+    public static void main(String[] args) {
+//        homeWorkFirstPoint();
+        homeWorkSecondPoint();
+    }
+
+
+    private static void homeWorkFirstPoint() {
+        String[] words = {
+                "коллекции",
+                "представляют",
+                "собой",
+                "контейнеры",
+                "которые",
+                "хранят",
+                "в",
+                "себе",
+                "множество",
+                "элементов",
+                "и",
+                "позволяют",
+                "работать",
+                "с",
+                "этой",
+                "группой",
+                "элементов",
+                "как",
+                "с",
+                "одним",
+                "объектов",
+                "коллекции",
+                "используются",
+                "для",
+                "хранения",
+                "получения",
+                "обработки",
+                "множества",
+                "объектов"
+        };
+        System.out.println("Исходный набор: ");
+        for (String s : words) {
+            System.out.print(s + ", ");
         }
-        catch (NumberFormatException e) {
-            System.out.println(e);
-        }
-        catch (NullPointerException e) {
-            System.out.println("Ошибка...");
-        }
+        System.out.println();
+        StringHandler stringHandler = new StringHandler(words);
+        System.out.println("Уникальные значения: ");
+        System.out.println(stringHandler.returnOnlyUnique());
+        System.out.println("Повторы слов: ");
+        System.out.println(stringHandler.countWordsAmount());
+    }
+
+
+    private static void homeWorkSecondPoint() {
 
     }
 }
